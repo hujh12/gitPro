386164c6b7ba5c1ec0f72a753dee07d33c91f849
hujh12 <1061707633@qq.com>
2020-09-05 17:17:11 +0800

    版本2

--- src/test.java
+++ src/test.java
@@ -1,6 +1,6 @@
 package com.demo;
 public class DmUserConsumerApplication {
-------
+------2
 	public static void main(String[] args) {
 		SpringApplication.run(DmUserConsumerApplication.class, args);
 	}
